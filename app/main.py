4a67f1c4d970cd4b3b90bf53fce804c846c72e95
Bano Rani <banor3106@gmail.com>
2026-07-06 07:04:07 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,32 +1,46 @@
 from fastapi import FastAPI
+
 from app.config import settings
 from app.database import Base, engine
+
 from app.models.user import User
 from app.models.company import Company
-from app.routers import auth
-from app.routers import companies
-from app.models.chat_memory import ChatMemory 
-from app.routers import chat
-from app.routers import prd
+from app.models.chat_memory import ChatMemory
 from app.models.prd import PRD
-from app.routers import pitchdeck
 from app.models.pitchdeck import PitchDeck
 
-#Base.metadata.create_all(bind=engine)
+from app.routers import auth
+from app.routers import companies
+from app.routers import chat
+from app.routers import prd
+from app.routers import pitchdeck
+
+# NEW
+from app.services.qdrant_service import create_collection
+
+
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version="1.0.0"
 )
 
+
 @app.on_event("startup")
 def startup():
+
+    # Create PostgreSQL tables
     Base.metadata.create_all(bind=engine)
+
+    # Create Qdrant collection (runs only if it doesn't already exist)
+    create_collection()
+
 
 app.include_router(auth.router)
 app.include_router(companies.router)
 app.include_router(chat.router)
 app.include_router(prd.router)
 app.include_router(pitchdeck.router)
+
 
 @app.get("/")
 def root():
