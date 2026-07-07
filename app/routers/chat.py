124d9b7933c543f0c14c4c05d53855946035b8ce
Bano Rani <banor3106@gmail.com>
2026-07-07 15:50:40 +0500

    Update chat.py

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -3,9 +3,11 @@ from sqlalchemy.orm import Session
 
 from app.database import get_db
 from app.models.user import User
+from app.models.chat import ChatMessage
 from app.schemas.chat import ChatRequest, ChatResponse
 from app.security import get_current_user
 from app.services.ai_service import chat_with_ai
+
 
 router = APIRouter(
     prefix="/chat",
@@ -19,9 +21,6 @@ def chat(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
-    """
-    Chat with Solirna AI.
-    """
 
     response = chat_with_ai(
         db=db,
@@ -32,3 +31,33 @@ def chat(
     return ChatResponse(
         response=response
     )
+
+
+@router.delete("/")
+def delete_chat(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+
+    db.query(ChatMessage).filter(
+        ChatMessage.user_id == current_user.id
+    ).delete()
+
+    db.commit()
+
+    return {
+        "message": "Chat history deleted successfully"
+    }
+    @router.get("/")
+def get_chat_history(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+
+    messages = db.query(ChatMessage).filter(
+        ChatMessage.user_id == current_user.id
+    ).all()
+
+    return {
+        "messages": messages
+    }
